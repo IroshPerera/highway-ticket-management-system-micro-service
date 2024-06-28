0c7eef634b9b585ffa7323ea38f74ed6c31af9d5
Irosh Perera <iroshlashan31@gmail.com>
2024-06-28 23:55:24 +0530

    few changes

--- ticket-vehicle-service/src/main/java/lk/ijse/ticket_vehicle_service/util/exception/UserServiceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.ticket_vehicle_service.util.exception;
-
-public class UserServiceNotFoundException extends RuntimeException{
-    public UserServiceNotFoundException(String message) {
-        super(message);
-    }
-
-
-}
