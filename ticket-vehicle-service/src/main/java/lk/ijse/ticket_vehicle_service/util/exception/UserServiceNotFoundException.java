15e014e92c11e77e376249c49fb7d88a9985e3a1
Irosh Perera <iroshlashan31@gmail.com>
2024-06-28 22:40:18 +0530

    implement vehicle utilities

---
+++ ticket-vehicle-service/src/main/java/lk/ijse/ticket_vehicle_service/util/exception/UserServiceNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.ticket_vehicle_service.util.exception;
+
+public class UserServiceNotFoundException extends RuntimeException{
+    public UserServiceNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
