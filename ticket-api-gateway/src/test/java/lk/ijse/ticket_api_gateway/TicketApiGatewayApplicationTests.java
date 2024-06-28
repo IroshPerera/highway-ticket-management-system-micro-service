76895c136878cb9225709ddb1e0450de28120f07
Irosh Perera <iroshlashan31@gmail.com>
2024-06-28 23:57:14 +0530

    implement api gateway

---
+++ ticket-api-gateway/src/test/java/lk/ijse/ticket_api_gateway/TicketApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticket_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
