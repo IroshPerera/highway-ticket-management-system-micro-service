c3f79a7b8cad9624d4344a6c2e9d542698473392
Irosh Perera <iroshlashan31@gmail.com>
2024-06-28 22:39:42 +0530

    implement user utilities

---
+++ ticket-user-service/src/main/java/lk/ijse/ticket_user_service/util/map/Mapper.java
@@ -0,0 +1,29 @@
+package lk.ijse.ticket_user_service.util.map;
+
+import lk.ijse.ticket_user_service.dto.UserDTO;
+import lk.ijse.ticket_user_service.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Mapper {
+    private final ModelMapper map;
+
+
+    public UserDTO mapToUserDTO(UserEntity userEntity){
+        return map.map(userEntity, UserDTO.class);
+    }
+
+    public UserEntity mapToUserEntity(UserDTO userDTO){
+        return map.map(userDTO, UserEntity.class);
+    }
+
+
+    public List<UserDTO> mapToUserDTOs(List<UserEntity> userEntities) {
+        return map.map(userEntities, List.class);
+    }
+}
